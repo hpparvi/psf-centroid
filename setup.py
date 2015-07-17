b5d4d9300130e2ebc51ca81fd16e8c14688a0306
Hannu Parviainen <hpparvi@gmail.com>
2015-07-17 12:23:32 +0100

    Changed the name of the package to pyfc, yikes.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from numpy.distutils.core import setup, Extension
 from numpy.distutils.misc_util import Configuration
 import distutils.sysconfig as ds
 
-setup(name='PyCntr',
+setup(name='PyFastCentroid',
       version='0.1',
-      description='Fast centroiding routines for Python.',
+      description='Fast PSF centroiding routines for Python.',
       author='Hannu Parviainen',
       author_email='hpparvi@gmail.com',
       url='',
-      package_dir={'pycntr':'src'},
-      packages=['pycntr'],
-      ext_modules=[Extension('pycntr.cntrf', ['src/cntr.f90','src/cntr.pyf'], libraries=['gomp','m'])]
+      package_dir={'pyfc':'src'},
+      packages=['pyfc'],
+      ext_modules=[Extension('pyfc.cntrf', ['src/cntr.f90','src/cntr.pyf'], libraries=['gomp','m'])]
      )
